70008bb78febe1ed59dd36fc75497688f676f195
AbdelrahmanMakhoul <159889663+AbdelrahmanMakhoul@users.noreply.github.com>
2024-02-13 16:10:50 +0300

    Add files via upload

---
+++ basic data type.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int main()
+{
+int n;
+long long l;
+char ch;
+float f;
+double d;
+cin >> n >> l >> ch >> f >> d;
+cout << n << "\n" << l << "\n" << ch << "\n" << f << "\n" << d << "\n" ;
+
+
+}
